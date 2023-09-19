1db397f37da62d925fdb54d5990c1c20d57861fa
Федцов Кирилл <Fedtsov.K@ecostandard.ru>
2023-09-19 19:01:47 +0300

    added colored maze solver

--- ThePrimeagen_Algoritms_course/Recursion/maze_traversal.py
+++ ThePrimeagen_Algoritms_course/Recursion/maze_traversal.py
@@ -1,90 +1,88 @@
-input = [line.strip() for line in open("input.txt", "r")][::-1] 
+from copy import deepcopy, copy
 
-def find_start_and_end(input) -> tuple:
-    for x, row in enumerate(input):
-        for y, symbol in enumerate(row):
-            if symbol == "S":
-                start = (x, y)
-            if symbol == "E":
-                end = (x, y)
+maze = []
+visited = []
+road = []
 
-    return start, end
+with open("maze.txt", "r") as file:
+    for line in file:
+        maze.append([letter for letter in line])
+        visited.append([False for char in range(len(line))])
+
+start = "S"
+finish = "F"
+wall = "#"
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
-def walk(
-        map: list[tuple], 
-        current: tuple, 
-        end: tuple, 
-        seen: list[list[bool]],
-        wall: str = "#", 
-        result: list = []
-        ):
-    
-    result.append(current)
-    x, y = current
-
-    try:
-        # 1. out of bounds
-        what = map[x][y]
-
-        # 2. it's a wall
-        if map[x][y] == wall:
-            return False
-    
-        # 3. it's the end
-        elif map[x][y] == "E":
+def is_not_out_of_bounds(x, y):
+    if 0 <= x < len(maze):
+        if 0 <= y < len(maze[0]):
             return True
-    
-        # 4. it's been visited already
-        elif seen[x][y] == True:
-            return False
+    return False
 
-    except IndexError:
+
+def traverse(x, y, maze, visited):
+    if not is_not_out_of_bounds(x, y):
+        print(x, y)
+        print("is out of bounds\n")
         return False
 
+    elif maze[x][y] == wall:
+        print(x, y)
+        print("is wall\n")
+        return False
 
-    seen[x][y] = True
+    elif visited[x][y] == True:
+        print(x, y)
+        print("is visited already\n")
+        return False
 
-    moves = [
-            (1, 0),
-            (0, 1),
-            (-1, 0),
-            (0, -1)
-            ]
+    elif maze[x][y] == finish:
+        print(x, y)
+        print("Found finish line!!!!!!!!!!!!!!!!\n")
+        road.append((x, y))
+        return True
 
-    for a, b in moves:
-        x, y = result[-1]
-        x, y = x + a, y + b
-        if not walk(map, (x,y), end, seen, wall, result):
-            result.pop()
-        else:
-            return result
+    print(x, y)
+    print("is OK. Going further\n")
+    visited[x][y] = True
+    road.append((x, y))
+
+    for direction in directions:
+        dir_x, dir_y = direction
+        new_x, new_y = x + dir_x, y + dir_y
+        if traverse(new_x, new_y, maze, visited=visited):
+            return True
+
+    popped = road.pop()
+    print(f"Just popped this {popped}")
 
 
-def print_result_map(input):
-    result_map = []
-    for x, row in enumerate(input):
-        result_row = ""
-        for y, symbol in enumerate(row):
-            if (x, y) in result:
-                result_row += "o"
-            else:
-                result_row += symbol
+def main(x, y):
+    result = traverse(x, y, maze, visited)
+    print(result)
 
-        result_map.append(result_row)
 
-    print("\nThe input:")
-    [print(row) for row in input[::-1]]
-    print("\nThe result:")
-    [print(row) for row in result_map[::-1]]
-    
+def print_path(a_maze=[line[:] for line in maze], road=road):
+    for position in road:
+        x, y = position
+        a_maze[x][y] = "\033[92m@\033[0m"
 
-if __name__=="__main__":
-    seen = [[False for i in range(len(input[0]))] for i in range(len(input))]
+    for line in a_maze:
+        print("".join(line).strip())
 
-    start, end = find_start_and_end(input)
 
-    result = (walk(input, start, end, seen, "#", []))
+def find_start(maze=maze):
+    for x in range(len(maze)):
+        for y in range(len(maze[x])):
+            if maze[x][y] == "S":
+                return (x, y)
 
-    print(f"Resulting path is {result}")
-    print_result_map(input)
+
+if __name__ == "__main__":
+    x, y = find_start()
+    result = main(x, y)
+    print("Here's the path: ", road)
+    print_path()
+    # print(maze)
